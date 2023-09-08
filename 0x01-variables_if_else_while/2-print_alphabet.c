746efc41d4002980bc2f9fc2ac80cea0025422b3
larinegit <larineomoit04@gmail.com>
2023-09-08 08:29:04 -0700

    task1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet
+ *
+ * Return: always 0 (succss)
+ *
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar("\n");
+	return (0);
+
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints number between 0 to 9 and letters between a to f
+ *
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i <= 102; i++)
+	{
+		putchar(i);
+	}
+	putchar(i);
+	return (0);
+}
