858f5b7346372c09cf529389cf1d564b28108384
larinegit <larineomoit04@gmail.com>
2023-10-09 09:23:56 -0700

    task3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#include "3-function_like_macro.h"
-#include "3-function_like_macro.h"
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) ((x) < 0 ? -(x) : (x))
+#define ABS(x) ((x) < (0) ? -(x) : (x))
 
 #endif
