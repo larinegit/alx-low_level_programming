4c4d71724b631f79f39977522128fc855a5d59c0
larinegit <larineomoit04@gmail.com>
2023-10-20 06:07:41 -0700

    task0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,19 +9,18 @@
 size_t print_list(const list_t *h);
 
 {
-        size_t element;
-        elemnet = 0;
-        while (h != NULL)
-        {
-                if (h->str == NULL)
-                        printf("[%d], %s\n", 0,"(nil)");
-                else
-                        printf("[%d], %s\n", h->len, h->str);
-                h = h->next;
-                        element++
+	size_t element;
 
-        }
-        return (element)
+	elemnet = 0;
 
-
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d], %s\n", 0, "(nil)");
+		else
+			printf("[%d], %s\n", h->len, h->str);
+		h = h->next;
+		element++
+	}
+	return (element)
 }
