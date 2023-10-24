21c27fd19f9dd341c4aa273f6a6357313da49d41
larinegit <larineomoit04@gmail.com>
2023-10-24 08:29:23 -0700

    task101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,55 +1,88 @@
 #include "lists.h"
 #include <stdio.h>
 
+size_t looped_listint_len(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
+
 /**
- * print_listint_safe - function that prints a listint_t linked list.
- * @head: pointer to head of a list.
+ * looped_listint_len - Counts the number of unique nodes
+ * in a looped listint_t linked list.
+ * @head: A pointer to the head of the listint_t to check.
  *
- * Return: Length of list (INT)
+ * Return: If the list is not looped - 0.
+ * Otherwise - the number of unique nodes in the list.
+ */
+size_t looped_listint_len(const listint_t *head)
+{
+ const listint_t *larine, *mathew;
+ size_t nodes = 1;
+
+ if (head == NULL || head->next == NULL)
+ return (0);
+
+ larine = head->next;
+ mathew = (head->next)->next;
+
+ while (mathew)
+ {
+ if (larine == mathew)
+ {
+ larine = head;
+ while (larine != mathew)
+ {
+ nodes++;
+ larine = larine->next;
+ mathew = mathew->next;
+ }
+
+ larine = larine->next;
+ while (larine != mathew)
+ {
+ nodes++;
+larine = larine->next;
+ }
+
+ return (nodes);
+ }
+
+ larine = larine->next;
+mathew = (mathew->next)->next;
+ }
+
+ return (0);
+}
+
+/**
+ * print_listint_safe - Prints a listint_t list safely.
+ * @head: A pointer to the head of the listint_t list.
+ *
+ * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *slow_p = head ,*fast_p = head;
-	size_t ele = 0;
-	int is_loop = 0;
+ size_t nodes, index = 0;
 
-	while (slow_p && fast_p && fast_p->next)
-	{
-		if (!(fast_p->next->next))
-			break;
-		slow_p = slow_p->next;
-		fast_p = fast_p->next->next;
-		if (slow_p == fast_p)
-		{
-			slow_p = slow_p->next;
-			is_loop = 1;
-			break;
-		}
-	}
+ nodes = looped_listint_len(head);
 
-	if (!is_loop)
-	{
-		while (head)
-		{
-			ele++;
-			printf("[%p] %d\n", (void *)head, head->n);
-			head = head->next;
-		}
-		return (ele);
-	}
+ if (nodes == 0)
+ {
+ for (; head != NULL; nodes++)
+ {
+ printf("[%p] %d\n", (void *)head, head->n);
+ head = head->next;
+ }
+ }
 
-	while (head)
-	{
-		ele++;
-		if (head == slow_p)
-		{
-			printf("[%p] %d\n", (void *)head, head->n);
-			printf("-> [%p] %d\n", (void *)head, head->next->n);
-			exit(98);
-		}
+ else
+ {
+ for (index = 0; index < nodes; index++)
+ {
+ printf("[%p] %d\n", (void *)head, head->n);
+ head = head->next;
+ }
 
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
-	}
-	return (0);
+ printf("-> [%p] %d\n", (void *)head, head->n);
+ }
+
+ return (nodes);
 }
