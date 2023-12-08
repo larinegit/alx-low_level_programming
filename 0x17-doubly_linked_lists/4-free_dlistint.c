4aad818b5f743f35a317a11365c636c2b269cdcc
larinegit <larineomoit04@gmail.com>
2023-12-08 09:29:22 -0800

    tasks

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,19 +1,20 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef struct dlistint_s {
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
 
-/*
- * free_dlistint - Frees a linked dlistint_t list.
- * @head: The head of the dlistint_t list.
- */
-void free_dlistint(dlistint_t *head)
-{
-    dlistint_t *tmp;
+void free_dlistint(dlistint_t *head) {
+    dlistint_t *current = head;
+    dlistint_t *temp;
 
-
-    while (head)
-    {
-        tmp = head->next;
-        free(head);
-        head = tmp;
+    while (current != NULL) {
+        temp = current;
+        current = current->next;
+        free(temp);
     }
 }
